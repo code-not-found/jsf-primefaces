067c6be17e707ca4b298bd5549e87843b200137e
codenotfound <codenotfound.com@gmail.com>
2017-12-30 10:20:11 +0100

    add readme + update versions + links

---
+++ jsf-primefaces-websphere-application-server/README.md
@@ -0,0 +1,5 @@
+# jsf-primefaces-websphere-application-server
+
+A detailed step-by-step tutorial in which we build and run a Hello World PrimeFaces example using WebSphere Application Server and Maven.
+
+[https://www.codenotfound.com/jsf-primefaces-hello-world-example-websphere-application-server-maven.html](https://www.codenotfound.com/jsf-primefaces-hello-world-example-websphere-application-server-maven.html)
--- jsf-primefaces-websphere-application-server/src/main/java/com/codenotfound/primefaces/HelloWorld.java
+++ jsf-primefaces-websphere-application-server/src/main/java/com/codenotfound/primefaces/HelloWorld.java
@@ -5,26 +5,26 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class HelloWorld {
 
-    private String firstName = "John";
-    private String lastName = "Doe";
+  private String firstName = "John";
+  private String lastName = "Doe";
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    public String showGreeting() {
-        return "Hello" + " " + firstName + " " + lastName + "!";
-    }
+  public String showGreeting() {
+    return "Hello" + " " + firstName + " " + lastName + "!";
+  }
 }
